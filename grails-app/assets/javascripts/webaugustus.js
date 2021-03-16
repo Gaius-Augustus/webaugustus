0394e627018c12857a74e6e584716fed820e5121
hmehlan <mehlan@uni-greifswald.de>
2021-03-16 14:57:44 +0100

    check and restrict upload file size to 200MB

--- grails-app/assets/javascripts/webaugustus.js
+++ grails-app/assets/javascripts/webaugustus.js
@@ -6,6 +6,44 @@ function toggle_visibility(id) {
         e.style.display = 'block';
 }
 
+function setElementVisibility(id, visible) {
+    var e = document.getElementById(id);
+    if (e) {
+        if (visible) {
+            e.style.display = 'block';
+        }
+        else {
+            e.style.display = 'none';
+        }
+    }    
+}
+
+// Check that the uploaded files don't exceed te size limit of 200 MB
+window.onload = function checkUploadFileSize() {
+    if (document.forms && document.forms.length > 0) {
+        if (!window.FileReader) {
+            console.log("The file API isn't supported on this browser yet.");
+            return;
+        }
+        document.forms[0].addEventListener('submit', function( evt ) {
+            const fileInput = document.querySelectorAll('input[type="file"]');
+            for (var i = 0; i < fileInput.length; i++) {
+                if (fileInput[i].files.length > 0 && fileInput[i].files[0]) {
+                    if (fileInput[i].files[0].size > 200 * 1024 * 1024) { // 200MB
+                        evt.preventDefault();
+                        setElementVisibility('commit-button', true);
+                        setElementVisibility('aug-spinner', false);
+                        alert("The selected file \r\n\"" + fileInput[i].files[0].name 
+                                + "\" \r\nexceed our maximal size for file upload from local harddrives via web browser. \r\n"
+                                + "Please select a smaller file or use the ftp/http web link file upload option.");
+                        return;
+                    }
+                }
+            }
+        }, false);
+    }
+}
+
 // Expandable content script from flooble.com.
 // For more information please visit:
 //   http://www.flooble.com/scripts/expand.php
